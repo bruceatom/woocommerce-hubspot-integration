9efc84b7da0fa7343da6c87e0443f790fa1b00d7
Bruce Harpham <bruce@localhost>
2026-09-23 09:24:09 -0400

    Expand sample ecommerce order model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
+import json
+
+
 def main():
-    print("WooCommerce -> HubSpot integration starting...")
-    
-    
+    with open("sample_data/order.json") as file:
+         order = json.load(file)
+      
+    customer = {
+        "firstname": order["billing"]["first_name"],
+        "lastname": order["billing"]["last_name"],
+        "email": order["billing"]["email"],
+        "woocommerce_order_id": order["id"],
+        "order_total": order["total"],
+        "currency": order["currency"],
+    }
+     
+    print(customer)
+              
 if __name__ == "__main__":
     main()
-    
